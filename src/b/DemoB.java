cb22e782dbc0a648346c5acb33222c5602bca2f6
noort <noort@DESKTOP-KKE0VB6>
2024-01-03 20:50:22 -0500

    Initial Commit

---
+++ src/b/DemoB.java
@@ -0,0 +1,12 @@
+package b;
+
+public class DemoB {
+	public static void main(String[] args) {
+		System.out.println("Tuesday");
+
+		System.out.println("Day1");
+		System.out.println("Day2");
+
+	
+	}
+}
